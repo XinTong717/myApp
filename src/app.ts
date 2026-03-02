f0dae7a0d2ccfc028839bb1fb60c80c06d24311b
XinTong717 <xtong717@gmail.com>
2026-03-02 15:37:34 -0800

    chore: init taro weapp scaffold

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
+import Taro, { useLaunch } from '@tarojs/taro'
 
 import './app.scss'
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
     console.log('App launched.')
+    if (process.env.TARO_ENV === 'weapp') {
+      Taro.cloud.init({
+        env: 'cloud1-9g8njw4c79fb1322', // 你在云开发控制台创建环境后拿到的 envId
+      })
+    }
   })
 
   // children 是将要会渲染的页面
