d60c68f7705af39dd66f0a1ea3bcddee1ab5d117
Xin <95193271+XinTong717@users.noreply.github.com>
2026-04-21 08:58:14 -0700

    Add shared profile constants

---
+++ src/constants/profile.ts
@@ -0,0 +1,5 @@
+export const GENDER_OPTIONS = ['男', '女', '其他', '不想说'] as const
+export const AGE_RANGE_OPTIONS = ['18-25', '26-35', '36-45', '46-55', '55以上'] as const
+export const ROLE_OPTIONS = ['家长', '教育者', '同行者'] as const
+export const CHILD_AGE_OPTIONS = ['学龄前', '小学阶段', '中学阶段'] as const
+export const CHILD_STATUS_OPTIONS = ['寻找学习社区', '寻找同伴连接', '寻找项目活动', '寻找家庭支持', '自主探索中'] as const
