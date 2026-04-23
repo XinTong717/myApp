753360adda7847ee2ff382973901ef28834abac4
Xin <95193271+XinTong717@users.noreply.github.com>
2026-04-22 23:01:47 -0700

    Rename fallback cloud env build constant

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,3 +1,3 @@
 declare const __WEAPP_CLOUD_ENV_ID__: string
 declare const __WEAPP_RUNTIME_ENV__: string
-declare const __WEAPP_IS_SHARED_CLOUD_ENV__: boolean
+declare const __WEAPP_IS_FALLBACK_CLOUD_ENV__: boolean
