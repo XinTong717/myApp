6c6fedba56eb7c4288baa43c1c64ad85b8fcba7c
Xin <95193271+XinTong717@users.noreply.github.com>
2026-04-30 03:29:39 -0700

    chore: declare weapp build constants

--- types/global.d.ts
+++ types/global.d.ts
@@ -11,6 +11,10 @@ declare module '*.scss';
 declare module '*.sass';
 declare module '*.styl';
 
+declare const __WEAPP_CLOUD_ENV_ID__: string
+declare const __WEAPP_RUNTIME_ENV__: string
+declare const __WEAPP_IS_FALLBACK_CLOUD_ENV__: boolean
+
 declare namespace NodeJS {
   interface ProcessEnv {
     /** NODE 内置环境变量, 会影响到最终构建生成产物 */
@@ -23,7 +27,7 @@ declare namespace NodeJS {
      * @see https://taro-docs.jd.com/docs/next/env-mode-config#特殊环境变量-taro_app_id
      */
     TARO_APP_ID: string
+    /** 当前构建注入的云开发环境 ID */
+    TARO_APP_CLOUD_ENV?: string
   }
 }
-
-
