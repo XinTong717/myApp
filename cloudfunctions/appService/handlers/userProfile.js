ade0c7c47ec44dcc9bf3dd13b2cb87c32eb2a5d4
Xin <95193271+XinTong717@users.noreply.github.com>
2026-05-02 02:08:52 -0700

    feat: route account deletion request action

--- cloudfunctions/appService/handlers/userProfile.js
+++ cloudfunctions/appService/handlers/userProfile.js
@@ -4,6 +4,7 @@ module.exports = {
   getMe: userV2.getMe,
   saveProfile: userV2.saveProfile,
   updatePrivacySettings: userV2.updatePrivacySettings,
+  requestAccountDeletion: userV2.requestAccountDeletion,
   getSafetyOverview: userV2.getSafetyOverview,
   sendRequest: userV2.sendRequest,
   respondRequest: userV2.respondRequest,
