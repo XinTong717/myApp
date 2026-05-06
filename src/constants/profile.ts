592ed958671b960a414e14388138eb9cae1813ab
Xin <95193271+XinTong717@users.noreply.github.com>
2026-05-06 03:21:43 -0700

    fix: remove prefer not to say gender option

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -1,4 +1,4 @@
-export const GENDER_OPTIONS = ['男', '女', '其他', '不想说'] as const
+export const GENDER_OPTIONS = ['男', '女', '其他'] as const
 export const AGE_RANGE_OPTIONS = ['18-25', '26-35', '36-45', '46-55', '55以上'] as const
 export const ROLE_OPTIONS = ['家长', '教育者', '同行者'] as const
 export const CHILD_AGE_OPTIONS = ['学龄前', '小学阶段', '中学阶段'] as const
