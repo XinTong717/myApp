8c5c0babd48a97acb10cf33d8e09bdc8b794dfd7
Xin <95193271+XinTong717@users.noreply.github.com>
2026-05-09 00:41:17 -0700

    refactor: reuse privacy legal version

--- src/pkg/legal/privacy-policy/index.tsx
+++ src/pkg/legal/privacy-policy/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from '@tarojs/components'
 import AppPage from '../../../components/common/AppPage'
 import AppCard from '../../../components/common/AppCard'
+import { CURRENT_PRIVACY_VERSION } from '../../../constants/legal'
 import { palette } from '../../../theme/palette'
 import { space } from '../../../theme/spacing'
 import { typography } from '../../../theme/typography'
@@ -17,7 +18,7 @@ function Section(props: { title: string; children: any }) {
 }
 
 function BodyText(props: { children: any }) {
-  return <Text style={{ ...typography.meta, color: palette.subtext, lineHeight: '22px' }}>{props.children}</Text>
+  return <Text style={{ ...typography.meta, color: palette.subtext }}>{props.children}</Text>
 }
 
 export default function PrivacyPolicyPage() {
@@ -25,7 +26,7 @@ export default function PrivacyPolicyPage() {
     <AppPage>
       <View style={{ marginBottom: space(4) }}>
         <Text style={{ ...typography.title, color: palette.text }}>隐私政策</Text>
-        <View style={{ marginTop: space(1) }}><Text style={{ ...typography.micro, color: palette.subtext }}>更新日期：2026-05-08</Text></View>
+        <View style={{ marginTop: space(1) }}><Text style={{ ...typography.micro, color: palette.subtext }}>更新日期：{CURRENT_PRIVACY_VERSION}</Text></View>
       </View>
 
       <Section title='1. 适用范围'>
